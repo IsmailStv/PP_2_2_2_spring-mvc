2ce4a6f2233f726359f6ff664909b399a346c108
IsmailStv <erkenov@yandex.ru>
2023-08-07 19:46:51 +0300

    Замечания от ментора: CarServiceImpl    список машин, который ты создаешь - это своего рода “база данных”. Объяви в качестве поля и инициализируй отдельно от метода список машин, сейчас при каждом вызове метода (запросе на /cars) создается новый список машин. Согласно заданию, список из 5 машин, у тебя 7)
                          при именовании методов используй глаголы - carList -> getCarList
    
                          CarController
    
                          почему не используешь внедрение зависимостей спрингом, а явно создаешь объект через оператор new? Объяви поле типа UserService и в методе контроллера на объекте вызови метод сarList (getCarList)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,22 +1,32 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
-
-import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    private final CarService service;
+
+    public CarController(CarServiceImpl service) {
+        this.service = service;
+    }
 
     @GetMapping(value = "/cars")
-    public String printCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
-        CarServiceImpl carsServiceImpl = new CarServiceImpl();
-        List<Car> qtyCars = carsServiceImpl.carList(count);
-        model.addAttribute("cars", qtyCars);
-        return "cars";
+    public String printCar(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, ModelMap model) {
+        model.addAttribute("printCar", service.getCarList(count));
+        return "/cars";
     }
+//    @GetMapping(value = "/cars")
+//    public String printCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
+//        CarServiceImpl carsServiceImpl = new CarServiceImpl();
+//        List<Car> qtyCars = carsServiceImpl.getCarList(count);
+//        model.addAttribute("cars", qtyCars);
+//        return "cars";
+//    }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -8,19 +8,37 @@ import java.util.stream.Collectors;
 
 public class CarServiceImpl implements CarService {
 
-    @Override
-    public List<Car> carList(Integer count) {
-        List<Car> carList;
-        {
-            carList = new ArrayList<>();
-            carList.add(new Car("Renault", "Megan", 2004));
-            carList.add(new Car("Mercedes-Benz", "GLS", 2015));
-            carList.add(new Car("BMW", "X6", 2019));
-            carList.add(new Car("ВАЗ", "Калина", 2020));
-            carList.add(new Car("Toyota", "Corolla", 2018));
-            carList.add(new Car("Kia", "Mohave", 2021));
-            carList.add(new Car("Cadillac", "Escalade", 2023));
-        }
-        return carList.stream().limit(count).collect(Collectors.toList());
+    List<Car> carList = new ArrayList<>();
+
+    {
+        carList.add(new Car("Renault", "Megan", 2004));
+        carList.add(new Car("Mercedes-Benz", "GLS", 2015));
+        carList.add(new Car("BMW", "X6", 2019));
+        carList.add(new Car("ВАЗ", "Калина", 2020));
+        carList.add(new Car("Toyota", "Corolla", 2018));
     }
+    public CarServiceImpl() {
+    }
+
+    @Override
+    public List<Car> getCarList(Integer count) {
+        List<Car> newCarlist = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            newCarlist.add(carList.get(i));
+        }
+        return newCarlist;
+    }
+//    @Override
+//    public List<Car> carList(Integer count) {
+//        List<Car> carList;
+//        {
+//            carList = new ArrayList<>();
+//            carList.add(new Car("Renault", "Megan", 2004));
+//            carList.add(new Car("Mercedes-Benz", "GLS", 2015));
+//            carList.add(new Car("BMW", "X6", 2019));
+//            carList.add(new Car("ВАЗ", "Калина", 2020));
+//            carList.add(new Car("Toyota", "Corolla", 2018));
+//        }
+//        return carList.stream().limit(count).collect(Collectors.toList());
+//    }
 }
